7b4176d29a04f33ba1560ea4a5c74f40e5123695
alexander-p30 <alexanderp30e@gmail.com>
2021-08-09 21:03:24 -0300

    refactor: rename trello api adapter functions

--- lib/adapters/trello_api.ex
+++ lib/adapters/trello_api.ex
@@ -4,10 +4,11 @@ defmodule WeeklyTrello.Adapters.TrelloApi do
   plug(Tesla.Middleware.BaseUrl, "https://api.trello.com/1/cards")
   plug(Tesla.Middleware.JSON)
 
-  def req(payload) do
-    Enum.map(payload, fn card_params ->
-      post("", card_params, query: [key: trello_api_key(), token: trello_api_token()])
-    end)
+  def create_cards([_ | _] = cards_params), do: Enum.map(cards_params, &create_card/1)
+  def create_cards(%{} = card_params), do: Enum.map([card_params], &create_card/1)
+
+  def create_card(card_params) do
+    post("", card_params, query: [key: trello_api_key(), token: trello_api_token()])
   end
 
   defp trello_api_key(), do: Application.get_env(:weekly_trello, :api_key)
